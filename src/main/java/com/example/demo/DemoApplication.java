43a01c36d6d573b586f24ef3454ffdab8e07bede
harsha <harsha.medipalli123@gmail.com>
2021-05-20 20:01:43 +0530

    added Servlet initializer

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,13 +2,23 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class DemoApplication {
+public class DemoApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-		System.out.println("hello");
-	}
+	@Override
+    protected SpringApplicationBuilder configure(
+      SpringApplicationBuilder builder) {
+        return builder.sources(DemoApplication.class);
+    }
+
+    public static void main(String[] args) {
+        SpringApplication sa = new SpringApplication(
+        		DemoApplication.class);
+        sa.run(args);
+    }
+
 
 }
